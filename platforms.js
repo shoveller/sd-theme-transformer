ecef725ab2efe9b0ecdb0ef1db0d00623dd68aa8
cinos81 <cinos81@gmail.com>
2023-02-11 13:44:51 +0900

    feat: add transformGroups

--- platforms.js
+++ platforms.js
@@ -1,54 +1,59 @@
-const {px_rem} = require("./transforms/px_rem");
-const {sizes_px} = require("./transforms/sizes_px");
-const {typography_shorthand} = require("./transforms/typography_shorthand");
-const {typography_properties} = require("./transforms/typography_properties");
-const {shadow_shorthand} = require("./transforms/shadow_shorthand");
-const {shadow_properties} = require("./transforms/shadow_properties");
-const {name_symbol} = require("./transforms/name_symbol");
-const {css_variables} = require("./formats/css_variables");
-const {javascript_cjs} = require("./formats/javascript_cjs");
-const styleDictionary = require('style-dictionary');
+const { px_rem } = require('./transforms/px_rem')
+const { sizes_px } = require('./transforms/sizes_px')
+const { typography_shorthand } = require('./transforms/typography_shorthand')
+const { typography_properties } = require('./transforms/typography_properties')
+const { shadow_shorthand } = require('./transforms/shadow_shorthand')
+const { shadow_properties } = require('./transforms/shadow_properties')
+const { name_symbol } = require('./transforms/name_symbol')
+const { css_variables } = require('./formats/css_variables')
+const { javascript_cjs } = require('./formats/javascript_cjs')
+const styleDictionary = require('style-dictionary')
 
 /**
  * 포메터 등록
  */
-styleDictionary.registerFormat(css_variables);
-styleDictionary.registerFormat(javascript_cjs);
+styleDictionary.registerFormat(css_variables)
+styleDictionary.registerFormat(javascript_cjs)
 
 /**
  * 트랜스포머 등록
  */
-styleDictionary.registerTransform(name_symbol);
-styleDictionary.registerTransform(sizes_px);
-styleDictionary.registerTransform(px_rem);
-styleDictionary.registerTransform(shadow_shorthand);
-styleDictionary.registerTransform(shadow_properties);
-styleDictionary.registerTransform(typography_shorthand);
-styleDictionary.registerTransform(typography_properties);
+styleDictionary.registerTransform(name_symbol)
+styleDictionary.registerTransform(sizes_px)
+styleDictionary.registerTransform(px_rem)
+styleDictionary.registerTransform(shadow_shorthand)
+styleDictionary.registerTransform(shadow_properties)
+styleDictionary.registerTransform(typography_shorthand)
+styleDictionary.registerTransform(typography_properties)
 
 /**
  *
  * @param {buildPath: string, destination: string, selector: string}
  * @returns {import('style-dictionary/types').Platform}
  */
-module.exports.makeCSSTheme = ({ buildPath, destination, selector = ':root' }) => ({
-    buildPath,
-    transforms: [
-        'attribute/cti',
-        'name/cti/kebab',
-        'sizes/px',
-        'shadow/shorthand',
-        'px/rem',
-        'typography/shorthand',
-    ],
-    files: [
-        {
-            destination,
-            format: 'css/variables',
-            selector,
-        },
-    ],
-});
+module.exports.makeCSSTheme = ({
+  buildPath,
+  destination,
+  selector = ':root',
+}) => ({
+  buildPath,
+  transformGroup: 'css',
+  transforms: [
+    'attribute/cti',
+    'name/cti/kebab',
+    'sizes/px',
+    'shadow/shorthand',
+    'px/rem',
+    'typography/shorthand',
+  ],
+  files: [
+    {
+      destination,
+      format: 'css/variables',
+      selector,
+    },
+  ],
+})
 
 /**
  *
@@ -56,23 +61,24 @@ module.exports.makeCSSTheme = ({ buildPath, destination, selector = ':root' }) =
  * @returns {import('style-dictionary/types').Platform}
  */
 module.exports.makeSCSSTheme = ({ buildPath, destination }) => ({
-    buildPath,
-    transforms: [
-        'attribute/cti',
-        'name/cti/kebab',
-        'sizes/px',
-        'shadow/shorthand',
-        'px/rem',
-        'typography/shorthand',
-        'name/symbol',
-    ],
-    files: [
-        {
-            destination,
-            format: `scss/variables`,
-        },
-    ],
-});
+  buildPath,
+  transformGroup: 'scss',
+  transforms: [
+    'attribute/cti',
+    'name/cti/kebab',
+    'sizes/px',
+    'shadow/shorthand',
+    'px/rem',
+    'typography/shorthand',
+    'name/symbol',
+  ],
+  files: [
+    {
+      destination,
+      format: `scss/variables`,
+    },
+  ],
+})
 
 /**
  *
@@ -80,15 +86,22 @@ module.exports.makeSCSSTheme = ({ buildPath, destination }) => ({
  * @returns {import('style-dictionary/types').Platform}
  */
 module.exports.makeESMTheme = ({ buildPath, destination }) => ({
-    buildPath,
-    transforms: ['name/symbol', 'sizes/px', 'px/rem', 'shadow/properties', 'typography/properties'],
-    files: [
-        {
-            destination,
-            format: `javascript/es6`,
-        },
-    ],
-});
+  buildPath,
+  transformGroup: 'esm',
+  transforms: [
+    'name/symbol',
+    'sizes/px',
+    'px/rem',
+    'shadow/properties',
+    'typography/properties',
+  ],
+  files: [
+    {
+      destination,
+      format: `javascript/es6`,
+    },
+  ],
+})
 
 /**
  *
@@ -96,12 +109,18 @@ module.exports.makeESMTheme = ({ buildPath, destination }) => ({
  * @returns {import('style-dictionary/types').Platform}
  */
 module.exports.makeCJSTheme = ({ buildPath, destination }) => ({
-    buildPath,
-    transforms: ['sizes/px', 'px/rem', 'shadow/properties', 'typography/properties'],
-    files: [
-        {
-            destination,
-            format: `javascript/cjs`,
-        },
-    ],
-});
+  buildPath,
+  transformGroup: 'cjs',
+  transforms: [
+    'sizes/px',
+    'px/rem',
+    'shadow/properties',
+    'typography/properties',
+  ],
+  files: [
+    {
+      destination,
+      format: `javascript/cjs`,
+    },
+  ],
+})
